5abe399097c22aac838dee0e8e7c39a68fd5adba
Виктор Ли <lee.vitya28@gmail.com>
2020-11-30 12:54:40 +0600

    Animation Fixed

---
+++ CharacterAnimation/CharacterAnimation/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  CharacterAnimation
+//
+//  Created by Victor Lee on 11/30/20.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    let characters = Array("VictorLee")
+    @State private var enable = false
+    @State private var dragValue = CGSize.zero
+    
+    var body: some View {
+        
+        HStack(spacing: 0) {
+            ForEach(0..<characters.count) { num in
+                Text(String(characters[num]))
+                    .padding(6)
+                    .font(.title)
+                    .background(enable ? Color.getDefaultColor1() : Color.getDefaultColor2())
+                    .offset(dragValue)
+                    .foregroundColor(.white)
+                    .animation(Animation.default.delay(Double(num) / 10))
+            }
+        }.gesture(
+            DragGesture()
+                .onChanged { dragValue = $0.translation }
+                .onEnded { _ in
+                    dragValue = .zero
+                    enable.toggle()
+                }
+        )
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+extension Color {
+    
+    static
+    func getDefaultColor1() -> Color {
+        return Color.init(red: 0.40, green: 0.49, blue: 1.92)
+    }
+
+    static
+    func getDefaultColor2() -> Color {
+        return Color.init(red: 2.84, green: 0.43, blue: 0.46)
+    }
+}
+
